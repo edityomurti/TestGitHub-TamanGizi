91ef1cfdb200252d00f4192362b07d3f00350ddd
edityomurti <swakresnaedityo@yahoo.com>
2015-10-31 20:43:55 +0700

    part 4 recycle view dari Slidenerd: stuck di getActivity() karena bukan exstends Fragment

--- app/src/main/java/com/example/sikikan/recycleviewtest/MainActivity.java
+++ app/src/main/java/com/example/sikikan/recycleviewtest/MainActivity.java
@@ -1,13 +1,46 @@
 package com.example.sikikan.recycleviewtest;
 
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    private RecyclerView recyclerView;
+    private EdityoAdapter adapter;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+        View layout = inflater.inflate(R.layout.activity_main, container, false);
+        recyclerView = (RecyclerView) layout.findViewById(R.id.drawerList);
+        adapter = new EdityoAdapter(startActionMode(getData()));
+        return layout;
+    }
+
+    public static List<Information> getData(){
+        List<Information> data = new ArrayList<>();
+        int[] icons = {R.drawable.chuck_norris, R.drawable.haji_lulung};
+        String[] titles = {"Chucky", "Lulungs"};
+        for (int i=0; i<titles.length && i<icons.length; i++)
+        {
+            Information current= new Information();
+            current.iconId=icons[i];
+            current.title= titles[i];
+            data.add(current);
+        }
+        return data;
     }
 }
